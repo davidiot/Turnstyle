ce9d1dda6d149e4b988b7e6a6958008d2878767a
davidiot <xdavidzhou@gmail.com>
2017-07-05 15:14:36 -0400

    added backligth

--- Turnstyle/Turnstyle.ino
+++ Turnstyle/Turnstyle.ino
@@ -1,6 +1,8 @@
 #include <CurieIMU.h>
 #include <MadgwickAHRS.h>
 #include <NewPing.h>
+#include <Wire.h>
+#include "rgb_lcd.h"
 
 // ULTRASONIC
 #define TRIGGER_PIN_1  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
@@ -14,14 +16,32 @@
 NewPing sonar_1(TRIGGER_PIN_1, ECHO_PIN_1, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 NewPing sonar_2(TRIGGER_PIN_2, ECHO_PIN_2, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
+// RGB
+rgb_lcd lcd;
+
 // MADGEWICK
 Madgwick filter;
 unsigned long microsPerReading, microsPrevious;
 float accelScale, gyroScale;
 
+// THRESHOLDS
+const int thresh = 60;       // < 60 cm to sensor is considered passing through.
+const float openAngle = 10;  // door is considered open at 10 degrees.
+
+// INDICATOR
+const int ledPin =  13;      // the number of the LED pin
+
+// POPULATION
+int population = 0;
+
 // SETUP
 void setup() {
   Serial.begin(9600);
+
+  // configure LCD
+  lcd.begin(16, 2);
+  lcd.setRGB(255, 255, 255);
+  lcd.print("Population: 0   ");
 
   // start the IMU and filter
   CurieIMU.begin();
@@ -37,13 +57,15 @@ void setup() {
   // initialize variables to pace updates to correct rate
   microsPerReading = 1000000 / 25;
   microsPrevious = micros();
+
+  // get initial angle of the door
+  // startAngle = measureYaw();
+  
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);  // Indicate that setup is complete
 }
 
 void loop() {
-  int aix, aiy, aiz;
-  int gix, giy, giz;
-  float ax, ay, az;
-  float gx, gy, gz;
   float doorAngle;
   int ping_1, ping_2;
   unsigned long microsNow;
@@ -51,33 +73,38 @@ void loop() {
   // check if it's time to read data and update the filter
   microsNow = micros();
   if (microsNow - microsPrevious >= microsPerReading) {
-
-    // read raw data from CurieIMU
-    CurieIMU.readMotionSensor(aix, aiy, aiz, gix, giy, giz);
-
-    // convert from raw data to gravity and degrees/second units
-    ax = convertRawAcceleration(aix);
-    ay = convertRawAcceleration(aiy);
-    az = convertRawAcceleration(aiz);
-    gx = convertRawGyro(gix);
-    gy = convertRawGyro(giy);
-    gz = convertRawGyro(giz);
-
-    // update the filter, which computes orientation
-    filter.updateIMU(gx, gy, gz, ax, ay, az);
-
-    // print the heading, pitch and roll
-    doorAngle = filter.getYaw();
-    Serial.print("Door Angle: ");
-    Serial.println(doorAngle);
+    float difference = measureYaw() - 180;
+    doorAngle = abs(difference);
 
     ping_1 = sonar_1.ping_cm();
     ping_2 = sonar_2.ping_cm();
-    printpings(ping_1, ping_2);
+    printinfo(doorAngle, ping_1, ping_2);
 
     // increment previous time, so we keep proper pace
     microsPrevious = microsPrevious + microsPerReading;
   }
+}
+
+float measureYaw() {
+  int aix, aiy, aiz;
+  int gix, giy, giz;
+  float ax, ay, az;
+  float gx, gy, gz;
+  // read raw data from CurieIMU
+  CurieIMU.readMotionSensor(aix, aiy, aiz, gix, giy, giz);
+
+  // convert from raw data to gravity and degrees/second units
+  ax = convertRawAcceleration(aix);
+  ay = convertRawAcceleration(aiy);
+  az = convertRawAcceleration(aiz);
+  gx = convertRawGyro(gix);
+  gy = convertRawGyro(giy);
+  gz = convertRawGyro(giz);
+
+  // update the filter, which computes orientation
+  filter.updateIMU(gx, gy, gz, ax, ay, az);
+
+  return filter.getYaw();
 }
 
 float convertRawAcceleration(int aRaw) {
@@ -98,8 +125,10 @@ float convertRawGyro(int gRaw) {
   return g;
 }
 
-void printpings(int ping_1, int ping_2) {
-  Serial.print("Ping 1: ");
+void printinfo(float doorAngle, int ping_1, int ping_2) {
+  Serial.print("Door Angle: ");
+  Serial.print(doorAngle);
+  Serial.print(" | Ping 1: ");
   Serial.print(ping_1); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.print(" cm | Ping 2: ");
   Serial.print(ping_2); // Send ping, get distance in cm and print result (0 = outside set distance range)
