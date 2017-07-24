5a17234ecf7f5fb8a194c4b1c09826deec181617
dannyzhou98 <daniel.zhou2012@gmail.com>
2017-07-24 11:00:04 -0400

    wip

--- Arduino/StreamingTest/StreamingTest.ino
+++ Arduino/StreamingTest/StreamingTest.ino
@@ -1,7 +1,14 @@
 #include "rgb_lcd.h"
+#include <CurieBLE.h>
 
 rgb_lcd lcd;
-int buttonPin = 4; 
+
+BLEPeripheral blePeripheral;
+BLEService turnstyleBleService("180D"); // just need a service that transmits a number
+
+BLEIntCharacteristic turnstyleBleIntCharacteristic("2A37", BLERead | BLENotify);
+
+int buttonPin = 4;
 int buzzerPin = 8;
 
 int beeps = 0;
@@ -9,6 +16,12 @@ boolean repLock = false; // prevents double counting
 
 void setup() {
   Serial.begin(9600);
+  blePeripheral.setLocalName("Turnstyle");
+  blePeripheral.setAdvertisedServiceUuid(turnstyleBleService.uuid());
+  blePeripheral.addAttribute(turnstyleBleService);
+  blePeripheral.addAttribute(turnstyleBleIntCharacteristic);
+  blePeripheral.begin();
+  Serial.println("Bluetooth device active, waiting for connections...");
 
   // configure LCD
   lcd.begin(16, 2);
@@ -16,23 +29,41 @@ void setup() {
   lcd.write("BEEPS: ");
 
   // configure pins
-  pinMode(buttonPin,INPUT);   //set button as digital input
-  pinMode(buzzerPin,OUTPUT);  //as buzzer as digital output
+  pinMode(buttonPin, INPUT);  //set button as digital input
+  pinMode(buzzerPin, OUTPUT); //as buzzer as digital output
 }
 
 void loop() {
-  if (digitalRead(buttonPin)) /*check button is pressed or not */ {
-    digitalWrite(buzzerPin, HIGH);  //pressed，then buzzer buzzes
-    if (!repLock) {
-      beeps++;
-      Serial.print(beeps);
-      Serial.println("*"); // we use * as a delimiter because there is some weird glitch with Node creating extra newlines
-      lcd.setCursor(7, 0);
-      lcd.print(beeps);
-      repLock = true;
+  // listen for BLE peripherals to connect:
+  BLECentral central = blePeripheral.central();
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+    // turn on the LED to indicate the connection:
+    digitalWrite(13, HIGH);
+    // as long as the central is still connected:
+    while (central.connected()) {
+      if (digitalRead(buttonPin)) { /*check button is pressed or not */
+        digitalWrite(buzzerPin, HIGH);  //pressed，then buzzer buzzes
+        if (!repLock) {
+          beeps++;
+          Serial.print(beeps);
+          Serial.println("*"); // we use * as a delimiter because there is some weird glitch with Node creating extra newlines
+          lcd.setCursor(7, 0);
+          lcd.print(beeps);
+          repLock = true;
+          turnstyleBleIntCharacteristic.setValue(beeps); 
+        }
+      } else {
+        digitalWrite(buzzerPin, LOW);//not pressed，then buzzer remains silent
+        repLock = false;
+      }
     }
-  } else {
-    digitalWrite(buzzerPin, LOW);//not pressed，then buzzer remains silent
-    repLock = false;
   }
+  // when the central disconnects, turn off the LED:
+  digitalWrite(13, LOW);
+  Serial.print("Disconnected from central: ");
+  Serial.println(central.address());
 }
